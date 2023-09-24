8e6ece612a6f0d6ed6d192e486191aae0f23ecab
molneya <62799417+molneya@users.noreply.github.com>
2023-09-24 20:31:54 +0800

    Convert BeatmapWindow to QMainWindow

--- GUI/BeatmapWindow.py
+++ GUI/BeatmapWindow.py
@@ -1,12 +1,12 @@
 
 from GUI.BeatmapTable import BeatmapTableView
-from PyQt6.QtWidgets import QWidget, QVBoxLayout
+from PyQt6.QtWidgets import QMainWindow, QVBoxLayout, QWidget
+from PyQt6.QtGui import QKeySequence
 from PyQt6.QtCore import Qt
 
-class BeatmapWindow(QWidget):
+class BeatmapWindow(QMainWindow):
     def __init__(self, main, index):
         super().__init__(parent=None)
-        self.main = main
         self.icon = main.icon
         self.collection = main.collectionDatabase.collections[index]
 
@@ -19,9 +19,19 @@ class BeatmapWindow(QWidget):
     def createLayout(self):
         layout = QVBoxLayout()
         self.createBeatmapTable(layout)
-        self.setLayout(layout)
+        self.createCentralWidget(layout)
+        self.createMenu()
 
     def createBeatmapTable(self, layout):
         self.beatmapTable = BeatmapTableView(self.collection)
         self.beatmapTable.refresh()
         layout.addWidget(self.beatmapTable)
+
+    def createCentralWidget(self, layout):
+        centralWidget = QWidget(self)
+        centralWidget.setLayout(layout)
+        self.setCentralWidget(centralWidget)
+
+    def createMenu(self):
+        fileMenu = self.menuBar().addMenu("File")
+        fileMenu.addAction("Exit", QKeySequence("Alt+F4"), self.close)
